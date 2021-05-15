f804b2b04a6e5c7747d8b67f8eab303d4da23baa
Aaron Cymor Nemeno <aaroncymor@gmail.com>
2021-05-15 12:37:56 +0800

    Replaced client with bot instead

--- discord/main.py
+++ discord/main.py
@@ -1,8 +1,10 @@
 import os
+import logging.config, logging
+
 import discord
+from discord.ext import commands
+from datetime import datetime
 import yaml
-import requests
-import json
 
 conf_env = os.getenv('CONFIG')
 if not conf_env:
@@ -14,19 +16,47 @@ with open(conf_env) as f:
 
 print(config_data)
 
-client = discord.Client()
+# load logger
+logging.config.dictConfig(config_data['logging'])
+logger = logging.getLogger('NinjakerDiscordLogger')
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+# load configuration file
+TOKEN = config_data['discord']['token']
+COMMAND_PREFIX = config_data['discord']['command_prefix']
+ALLOWED_CHANNELS = config_data['discord']['allowed_channels']
+ALLOWED_COMMANDS = config_data['discord']['allowed_commands']
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.content.startswith('hello'):
-        await message.channel.send('Hello!')
+bot = commands.Bot(command_prefix=COMMAND_PREFIX)
 
-client.run(config_data['app']['discord_token'])
+@bot.command()
+async def ping(ctx):
+    logger.debug("this is a test - pong")
+    await ctx.send('pong')
+
+bot.run(TOKEN)
+
+# client = discord.Client()
+
+# @client.event
+# async def on_ready():
+#     logger.debug('We have logged in as {0.user}'.format(client))
+
+# @client.event
+# async def on_message(message):
+#     username = str(message.author).split('#')[0]
+#     user_message = str(message.content)
+#     channel = str(message.channel.name)
+#     print(f'{username}: {user_message} ({channel})')
+
+#     if username == client.user:
+#         return
+
+#     if channel not in ALLOWED_CHANNELS:
+#         print('{0} not in allowed channels.'.format(channel))
+#         return
+
+#     if message.content.startswith('!hello'):
+#         await message.channel.send('Hello!')
+
+# client.run(config_data['discord']['token'])
 
