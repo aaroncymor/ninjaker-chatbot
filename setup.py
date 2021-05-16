be93dca98bd9a774074d47dc6f9df0ca8d99f59f
Aaron Cymor Nemeno <aaroncymor@gmail.com>
2021-05-16 14:50:57 +0800

    Fixing typo error in initializing setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 import setuptools
 
 #https://stackoverflow.com/questions/1054271/how-to-import-a-python-class-that-is-in-a-directory-above
-setuptools.setup(name='ninjnaker_chatbot')
+setuptools.setup(name='ninjaker_chatbot')
